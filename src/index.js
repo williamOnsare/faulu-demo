03f3d97bcc4b76025bcf57beb649f61b1ce05e94
William Onsare <william.onsare254@gmail.com>
2023-08-07 13:35:49 +0300

    migrate ReactDOM.render to new root API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const containerElem = document.getElementById("root");
+const root = createRoot(containerElem)
+root.render(<App />);
 
 reportWebVitals(sendToVercelAnalytics);
