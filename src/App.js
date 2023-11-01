05cc3253e528644cd1d05356b2cca743ec36a8af
William Onsare <william.onsare254@gmail.com>
2023-11-02 00:04:05 +0300

    testing embedded widget with `SameSite=Lax` attr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './Faulu brand.png';
-import './App.css';
+import logo from "./Faulu brand.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div style={{ marginTop: "2rem" }}>
+        <iframe
+          title="Qaswazi Support"
+          src="https://qaswazi.stage.chatsasa.com"
+          width="360"
+          height="640"
+        ></iframe>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
         <a
           className="App-link"
           href="https://www.faulukenya.com"
